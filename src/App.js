77a879a50427a44095d88c93f34cdb77b919a953
Laxmi Singh <155379742+ilaxmisingh@users.noreply.github.com>
2024-03-16 17:51:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// App.js
 
 import React from 'react';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import ProfilePage from './ProfilePage';
